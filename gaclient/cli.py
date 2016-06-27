a88a5be254b5918de4ead5fb6283d686ec995b5f
Jerome Kelleher <jk@well.ox.ac.uk>
2016-06-27 15:54:34 +0100

    Initial framework.

---
+++ gaclient/cli.py
@@ -0,0 +1,14 @@
+"""
+Command line interface for the GA4GH read data API.
+"""
+from __future__ import division
+from __future__ import print_function
+
+import gaclient
+
+def main():
+    client = gaclient.Client()
+
+
+if __name__ == "__main__":
+    main()
---
+++ gaclient/client.py
@@ -0,0 +1,11 @@
+"""
+Client API implementation of the draft GA4GH specification for bulk transfer
+of read data.
+"""
+from __future__ import division
+from __future__ import print_function
+
+
+
+
+
