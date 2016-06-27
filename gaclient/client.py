ae68ceac66c66bc6b725e2a25ee46f162ed2df02
Jerome Kelleher <jk@well.ox.ac.uk>
2016-06-27 17:05:53 +0100

    Initial working version of CH's server.

--- gaclient/client.py
+++ gaclient/client.py
@@ -5,7 +5,62 @@ of read data.
 from __future__ import division
 from __future__ import print_function
 
+import base64
+import os.path
+import sys
+
+import requests
 
 
+class Client(object):
+    """
+    Client for the GA4GH streaming API.
+    """
+    def __init__(
+            self, base_url=None, id_=None, reference_name=None, start=None,
+            end=None, output_file=None):
+        self.__base_url = base_url
+        self.__id = id_
+        self.__reference_name = reference_name
+        self.__start = start
+        self.__end = end
+        self.__chunk_size = 64 * 1024  # TODO add as CLI parameter
+        if output_file is None:
+            self.__output = sys.stdout
+        else:
+            self.__output = open(output_file, "wb")
+
+    def download(self):
+        """
+        Runs the query.
+        """
+        params = {}
+        if self.__reference_name is not None:
+            params["referenceName"] = self.__reference_name
+        if self.__start is not None:
+            params["start"] = self.__start
+        if self.__end is not None:
+            params["end"] = self.__end
+        url = os.path.join(self.__base_url, self.__id)
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+        ticket = response.json()
+        if "prefix" in ticket:
+            prefix = base64.b64decode(ticket["prefix"])
+            self.__output.write(prefix)
+        for url in ticket["urls"]:
+            print("Getting", url)
+            response = requests.get(url, stream=True)
+            response.raise_for_status()
+            for chunk in response.iter_content(self.__chunk_size):
+                self.__output.write(chunk)
+        if "suffix" in ticket:
+            prefix = base64.b64decode(ticket["suffix"])
+            self.__output.write(prefix)
 
 
+    def close(self):
+        """
+        Closes any open connections and files.
+        """
+        self.__output.close()
---
+++ gaclient_dev.py
@@ -0,0 +1,11 @@
+"""
+Development stub for gaclient CLI.
+"""
+from __future__ import division
+from __future__ import print_function
+
+import gaclient.cli
+
+
+if __name__ == "__main__":
+    gaclient.cli.main()
