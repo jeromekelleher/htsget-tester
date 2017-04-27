a4485bcdb066ce1f19b4b083ed70e728a1b65abe
AlexanderV <alexanderv@ebi.ac.uk>
2017-04-27 12:15:50 +0100

    batch test 8 files Sanger client vs Google server ... run nohup bash -x test_sanger_google.sh > ./result_sanger_google.txt 2>&1 &

---
+++ test_sanger_google.sh
@@ -0,0 +1,11 @@
+python tester.py -vv ENCFF000VWO.bam http://104.196.18.135/reads/CO_Ph7XUCRCSn-mRrofkhXY --client sanger-cli
+python tester.py -vv ENCFF284YOU.bam http://104.196.18.135/reads/CO_Ph7XUCRCi9Lasl7yY5Z0B --client sanger-cli
+
+python tester.py -vv NA12878.bam http://104.196.18.135/reads/CO_Ph7XUCRDW0-riiPr48fgB --client sanger-cli
+python tester.py -vv NA12878.cram http://104.196.18.135/reads/CO_Ph7XUCRCX3-no2-WqtqcB --client sanger-cli
+
+python tester.py -vv NA12891.bam http://104.196.18.135/reads/CO_Ph7XUCRCX3-no2-WqtqcB --client sanger-cli
+python tester.py -vv NA12891.cram http://104.196.18.135/reads/CO_Ph7XUCRDngbisk52h2KEB --client sanger-cli
+
+python tester.py -vv NA12892.bam http://104.196.18.135/reads/CO_Ph7XUCRCMtrWtwPmA4dgB --client sanger-cli
+python tester.py -vv NA12892.cram http://104.196.18.135/reads/CO_Ph7XUCRDnpcT9pKrW-tUB --client sanger-cli
