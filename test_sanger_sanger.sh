4a162f89218fd969509eac4a5ce4a955ad9a5817
AlexanderV <alexanderv@ebi.ac.uk>
2017-04-27 10:56:46 +0100

    batch 7 test files - Sanger client vs Sanger server

---
+++ test_sanger_sanger.sh
@@ -0,0 +1,13 @@
+
+python tester.py -vv ENCFF000VWO.bam http://52.213.144.237:9090/npg_ranger/ga4gh/v.0.1/get/sample/ENCFF000VWO --client sanger-cli
+python tester.py -vv ENCFF284YOU.bam http://52.213.144.237:9090/npg_ranger/ga4gh/v.0.1/get/sample/ENCFF284YOU_GRCH38 --client sanger-cli
+
+python tester.py -vv NA12878.bam http://52.213.144.237:9090/npg_ranger/ga4gh/v.0.1/get/sample/NA12878 --client sanger-cli
+python tester.py -vv NA12878.cram http://52.213.144.237:9090/npg_ranger/ga4gh/v.0.1/get/sample/NA12878_GRCH38 --client sanger-cli
+
+
+python tester.py -vv NA12891.bam http://52.213.144.237:9090/npg_ranger/ga4gh/v.0.1/get/sample/NA12891 --client sanger-cli
+python tester.py -vv NA12891.cram http://52.213.144.237:9090/npg_ranger/ga4gh/v.0.1/get/sample/NA12891_GRCH38 --client sanger-cli
+
+python tester.py -vv NA12892.bam http://52.213.144.237:9090/npg_ranger/ga4gh/v.0.1/get/sample/NA12892 --client sanger-cli
+python tester.py -vv NA12892.cram http://52.213.144.237:9090/npg_ranger/ga4gh/v.0.1/get/sample/NA12892_GRCH38 --client sanger-cli
