67c32fd7d68dc20b7421b33861be47ca1ceac0fd
alexanderv@ebi.ac.uk <alexanderv@ebi.ac.uk>
2017-05-09 11:15:16 +0100

    test suites for Dnanexus servers(AWS and Azure)

---
+++ test_dnanexus_azure.sh
@@ -0,0 +1,37 @@
+# do not exit on session disconnect
+trap "" HUP  
+
+#do not stop on errors
+set +e 
+
+if [[ "$@" == *fulltest ]]
+then
+	# remove last arg ( 'fulltest' )
+	set -- "${@:1:$(($#-1))}"	
+
+    set +x
+	echo ------------------------------------------
+    echo NOW TESTING BIG FILES...might take hours..
+    echo ------------------------------------------
+    set -x
+
+	python tester.py -vv NA12878.bam http://htsnexus.rnd.dnanex.us/v1/reads/Azure_BroadHiSeqX_b37/NA12878 "$@"
+	python tester.py -vv NA12878.cram http://htsnexus.rnd.dnanex.us/v1/reads/Azure_BroadHiSeqX_hg38/NA12878  "$@"
+
+	python tester.py -vv NA12891.bam http://htsnexus.rnd.dnanex.us/v1/reads/Azure_BroadHiSeqX_b37/NA12891  "$@"
+	python tester.py -vv NA12891.cram http://htsnexus.rnd.dnanex.us/v1/reads/Azure_BroadHiSeqX_hg38/NA12891  "$@"
+
+	python tester.py -vv NA12892.bam http://htsnexus.rnd.dnanex.us/v1/reads/Azure_BroadHiSeqX_b37/NA12892  "$@"
+	python tester.py -vv NA12892.cram http://htsnexus.rnd.dnanex.us/v1/reads/Azure_BroadHiSeqX_hg38/NA12892  "$@"
+fi
+
+set +x
+echo ------------------------------------------------------------
+echo Testing 2 small files ENCFF000VWO.bam and ENCFF284YOU.bam...
+echo ------------------------------------------------------------
+set -x
+
+python tester.py -vv ENCFF000VWO.bam http://htsnexus.rnd.dnanex.us/v1/reads/Azure_ENCODE/ENCFF284YOU  "$@"
+python tester.py -vv ENCFF284YOU.bam http://htsnexus.rnd.dnanex.us/v1/reads/Azure_ENCODE/ENCFF000VWO  "$@"
+
+set -e
