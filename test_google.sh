019e8920a7d4e05316847b0cb839cd8b6b13a29a
alexanderv@ebi.ac.uk <alexanderv@ebi.ac.uk>
2017-11-08 10:18:23 +0000

    updated test script for new htsget Google server(by Aaron Kemp) with token support

--- test_google.sh
+++ test_google.sh
@@ -1,6 +1,14 @@
 # do not exit on session disconnect
 trap "" HUP
 
+get_token()
+{
+        set +x
+        echo "Obtaining access token..."
+        read token < <(curl --request POST --data '{"token_uri":"https://www.googleapis.com/oauth2/v4/token","refresh_token":"1/A3OHTTywlfJAqCU95uxNYGx9a8PUhbZjhGC4O8A5_sM","client_secret":"T-hgpqETtzseRHYn6egZSPXK","client_id":"819467526948-ochlfup6bvc444455c7cirqovkikl7ik.apps.googleusercontent.com"}' https://developers.google.com/oauthplayground/refreshAccessToken | python -c "import sys, json; print json.load(sys.stdin)[\"access_token\"]")
+        set -x
+}
+
 #do not stop on errors
 set +e
 
@@ -15,14 +23,21 @@ then
     echo ------------------------------------------
     set -x
 
-	python tester.py -vv --filter-unmapped NA12878.bam http://104.196.18.135/reads/CO_Ph7XUCRDW0-riiPr48fgB "$@"
-	#python tester.py -vv --filter-unmapped NA12878.cram http://104.196.18.135/reads/CO_Ph7XUCRCX3-no2-WqtqcB "$@"
 
-	python tester.py -vv --filter-unmapped NA12891.bam http://104.196.18.135/reads/CO_Ph7XUCRDfpdqpjO3Tim0  "$@"
-	#python tester.py -vv --filter-unmapped NA12891.cram http://104.196.18.135/reads/CO_Ph7XUCRDngbisk52h2KEB "$@"
+    get_token
+	python tester.py -vv --filter-unmapped NA12878.bam  https://35.196.212.220/reads/ga4gh-demo/NA12878.bam --cert htsget-demo.pem  --bearer-token $token "$@"
+	get_token
+	python tester.py -vv --filter-unmapped NA12878.cram https://35.196.212.220/reads/ga4gh-demo/NA12878.cram --cert htsget-demo.pem  --bearer-token $token "$@"
 
-	python tester.py -vv --filter-unmapped NA12892.bam http://104.196.18.135/reads/CO_Ph7XUCRCMtrWtwPmA4dgB "$@"
-	#python tester.py -vv --filter-unmapped NA12892.cram http://104.196.18.135/reads/CO_Ph7XUCRDnpcT9pKrW-tUB "$@"
+    get_token
+	python tester.py -vv --filter-unmapped NA12891.bam https://35.196.212.220/reads/ga4gh-demo/NA12891.bam --cert htsget-demo.pem   --bearer-token $token "$@"
+	get_token
+	python tester.py -vv --filter-unmapped NA12891.cram https://35.196.212.220/reads/ga4gh-demo/NA12891.cram --cert htsget-demo.pem  --bearer-token $token "$@"
+
+	get_token
+	python tester.py -vv --filter-unmapped NA12892.bam https://35.196.212.220/reads/ga4gh-demo/NA12892.bam --cert htsget-demo.pem  --bearer-token $token "$@"
+	get_token
+	python tester.py -vv --filter-unmapped NA12892.cram https://35.196.212.220/reads/ga4gh-demo/NA12892.cram --cert htsget-demo.pem  --bearer-token $token "$@"
 fi
 
 
@@ -32,7 +47,9 @@ echo Testing 2 small files ENCFF000VWO.bam and ENCFF284YOU.bam...
 echo ------------------------------------------------------------
 set -x
 
-python tester.py -vv --filter-unmapped ENCFF000VWO.bam http://104.196.18.135/reads/CO_Ph7XUCRCSn-mRrofkhXY "$@"
-python tester.py -vv --filter-unmapped ENCFF284YOU.bam http://104.196.18.135/reads/CO_Ph7XUCRCi9Lasl7yY5Z0B "$@"
+get_token
+python tester.py -vv --filter-unmapped ENCFF000VWO.bam https://35.196.212.220/reads/ga4gh-demo/ENCFF000VWO.bam --cert htsget-demo.pem  --bearer-token $token "$@"
+get_token
+python tester.py -vv --filter-unmapped ENCFF284YOU.bam https://35.196.212.220/reads/ga4gh-demo/ENCFF284YOU.bam --cert htsget-demo.pem  --bearer-token $token "$@"
 
 set -e
