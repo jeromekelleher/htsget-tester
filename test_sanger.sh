25768e20b7b69058e11aaeaab400292d45529aa3
alexanderv@ebi.ac.uk <alexanderv@ebi.ac.uk>
2017-11-08 10:20:07 +0000

    token support for Sanger server test script

--- test_sanger.sh
+++ test_sanger.sh
@@ -1,6 +1,18 @@
 # do not exit on session disconnect
 trap "" HUP
 
+get_token()
+{
+        set +x
+        echo "Obtaining access token..."
+        read token < <(echo "A3Lw1EsFMJ1EvZTdjzq3nsR0qujwvzlF")
+        #curl -d "grant_type=password&client_id=f20cd2d3-682a-4568-a53e-4262ef54c8f4&client_secret=AMenuDLjVdVo4BSwi0QD54LL6NeVDEZRzEQUJ7hJOM3g4imDZBHHX0hNfKHPeQIGkskhtCmqAJtt_jm7EKq-rWw&username=ega-test-data@ebi.ac.uk&password=egarocks&scope=openid" -H "Content-Type: application/x-www-form-urlencoded" -k https://ega.ebi.ac.uk:8443/ega-openid-connect-server/token | python -c "import sys, json; print json.load(sys.stdin)[\"access_token\"]")
+        set -x
+}
+
+
+
+
 #do not stop on errors
 set +e
 
@@ -15,14 +27,20 @@ then
     echo ------------------------------------------
     set -x
 
-	python tester.py -vv NA12878.bam https://htsget.wtsi-npg-test.co.uk:9090/npg_ranger/ga4gh/sample/NA12878 "$@"
-	python tester.py -vv NA12878.cram https://htsget.wtsi-npg-test.co.uk:9090/npg_ranger/ga4gh/sample/NA12878_GRCH38 "$@"
+	get_token
+	python tester.py -vv NA12878.bam https://htsget.wtsi-npg-test.co.uk:9090/npg_ranger/ga4gh/sample/NA12878 --bearer-token $token "$@"
+	get_token
+	python tester.py -vv NA12878.cram https://htsget.wtsi-npg-test.co.uk:9090/npg_ranger/ga4gh/sample/NA12878_GRCH38 --bearer-token $token "$@"
 
-	python tester.py -vv NA12891.bam https://htsget.wtsi-npg-test.co.uk:9090/npg_ranger/ga4gh/sample/NA12891 "$@"
-	python tester.py -vv NA12891.cram https://htsget.wtsi-npg-test.co.uk:9090/npg_ranger/ga4gh/sample/NA12891_GRCH38 "$@"
+	get_token
+	python tester.py -vv NA12891.bam https://htsget.wtsi-npg-test.co.uk:9090/npg_ranger/ga4gh/sample/NA12891 --bearer-token $token "$@"
+	get_token
+	python tester.py -vv NA12891.cram https://htsget.wtsi-npg-test.co.uk:9090/npg_ranger/ga4gh/sample/NA12891_GRCH38 --bearer-token $token "$@"
 
-	python tester.py -vv NA12892.bam https://htsget.wtsi-npg-test.co.uk:9090/npg_ranger/ga4gh/sample/NA12892 "$@"
-	python tester.py -vv NA12892.cram https://htsget.wtsi-npg-test.co.uk:9090/npg_ranger/ga4gh/sample/NA12892_GRCH38 "$@"
+	get_token
+	python tester.py -vv NA12892.bam https://htsget.wtsi-npg-test.co.uk:9090/npg_ranger/ga4gh/sample/NA12892 --bearer-token $token "$@"
+	get_token
+	python tester.py -vv NA12892.cram https://htsget.wtsi-npg-test.co.uk:9090/npg_ranger/ga4gh/sample/NA12892_GRCH38 --bearer-token $token "$@"
 fi
 
 set +x
@@ -31,7 +49,9 @@ echo Testing 2 small files ENCFF000VWO.bam and ENCFF284YOU.bam...
 echo ------------------------------------------------------------
 set -x
 
-python tester.py -vv ENCFF000VWO.bam https://htsget.wtsi-npg-test.co.uk:9090/npg_ranger/ga4gh/sample/ENCFF000VWO "$@"
-python tester.py -vv ENCFF284YOU.bam https://htsget.wtsi-npg-test.co.uk:9090/npg_ranger/ga4gh/sample/ENCFF284YOU_GRCH38 "$@"
+get_token
+python tester.py -vv ENCFF000VWO.bam https://htsget.wtsi-npg-test.co.uk:9090/npg_ranger/ga4gh/sample/ENCFF000VWO --bearer-token $token "$@"
+get_token
+python tester.py -vv ENCFF284YOU.bam https://htsget.wtsi-npg-test.co.uk:9090/npg_ranger/ga4gh/sample/ENCFF284YOU_GRCH38 --bearer-token $token "$@"
 
 set -e
